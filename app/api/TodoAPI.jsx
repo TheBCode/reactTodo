5ac3aacbc03bad50a7d3a46e8169dd49762abf91
Byron Buyck <bbuyck1@gmail.com>
2017-12-16 10:50:03 -0500

    Add filter functionality and tests for TodoAPI suite

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -21,6 +21,33 @@ module.exports = {
 
         // check to see if todo is valid array
         return $.isArray(todos) ? todos : [];
+    },
+    filterTodos: function (todos, showCompleted, searchText) {
+        var filteredTodos = todos;
 
+        //Filter by showCompleted
+        filteredTodos = filteredTodos.filter((todo) => {
+            return !todo.completed || showCompleted;
+        });
+
+        //Filter by searchText
+        filteredTodos = filteredTodos.filter((todo) => {
+            var text = todo.text.toLowerCase();
+            return searchText.length === 0 || text.indexOf(searchText) > -1;
+        });
+
+
+        //Sort todos with non-completed 1st
+        filteredTodos.sort((a, b) => {
+           if (!a.completed && b.completed) {
+               return -1;
+           } else if (a.completed && !b.completed) {
+               return 1;
+           } else {
+               return 0;
+           }
+        });
+
+        return filteredTodos;
     }
 };
