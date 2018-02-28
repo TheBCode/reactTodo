890ce5aebf03f0ebe03a9a98d2b2076629b1c566
Byron Buyck <bbuyck1@gmail.com>
2018-02-27 22:18:53 -0500

    Fetch todos from Firebase on app start

--- app/actions/actions.jsx
+++ app/actions/actions.jsx
@@ -47,6 +47,26 @@ export var addTodos = (todos) => {
     };
 };
 
+export const startAddTodos = () => {
+    return (dispatch, getState) => {
+        const todosRef = firebaseRef.child('todos');
+
+        return todosRef.once('value').then((snapshot) =>{
+            const todos = snapshot.val() || {};
+            const parsedTodos = [];
+
+            Object.keys(todos).forEach((todoId) => {
+                parsedTodos.push({
+                    id: todoId,
+                    ...todos[todoId]
+                });
+            });
+
+            dispatch(addTodos(parsedTodos));
+        });
+    };
+};
+
 export var updateTodo = (id, updates) => {
     return {
         type: 'UPDATE_TODO',
