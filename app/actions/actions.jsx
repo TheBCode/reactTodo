f4dabd5f7e652c2398f41e641401d7e62111f2e9
Byron Buyck <bbuyck1@gmail.com>
2018-03-04 13:22:48 -0500

    Add auth reducer and actions

--- app/actions/actions.jsx
+++ app/actions/actions.jsx
@@ -89,6 +89,13 @@ export var startToggleTodo = (id, completed) => {
         });
     };
 };
+export var login = (uid) => {
+    return {
+        type: 'LOGIN',
+        uid
+    };
+};
+
 
 export var startLogin = () => {
     return (dispatch, getState) => {
@@ -97,6 +104,12 @@ export var startLogin = () => {
         }, (error) => {
             console.log('Unable to auth', error)
         });
+    };
+};
+
+export var logout = () => {
+    return {
+        type: 'LOGOUT'
     };
 };
 
